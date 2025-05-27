703cdea62dd181945ad1d91e0ce11c5191d927fc
nototria <humphrey0709@gmail.com>
2025-05-27 21:15:19 +0800

    add fetch path from server support

--- UI/app.py
+++ UI/app.py
@@ -8,7 +8,7 @@ selected_points = {}
 
 @app.route('/')
 def index():
-    return render_template("map.html")  # Load your HTML from templates folder
+    return render_template("map.html")
 
 @app.route('/submit_points', methods=['POST'])
 def submit_points():
@@ -16,6 +16,21 @@ def submit_points():
     selected_points = request.json
     print("Received points:", selected_points)
     return jsonify({"status": "success"})
+
+@app.route('/process_points', methods=['POST'])
+def process_points():
+    data = request.get_json()
+    start = data['start']
+    end = data['end']
+    
+    # structure holder
+    path = [
+        {"lat": start['lat'], "lng": start['lng']},
+        {"lat": (start['lat'] + end['lat']) / 2, "lng": (start['lng'] + end['lng']) / 2},
+        {"lat": end['lat'], "lng": end['lng']}
+    ]
+
+    return jsonify({"path": path})
 
 def run_app():
     app.run(debug=False, port=5000)
